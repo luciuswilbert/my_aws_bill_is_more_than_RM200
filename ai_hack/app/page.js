de8d08b9fa95521953d30e3874661f895b39c9b2
KwanJunEr <jonastheitguy@gmail.com>
2025-09-21 14:45:41 +0800

    push everything iahve first

--- ai_hack/app/page.js
+++ ai_hack/app/page.js
@@ -4,7 +4,6 @@ import { HeroSection } from "@/components/landingpage/HeroSection";
 import { Navigation } from "@/components/landingpage/Navigation";
 import { StatsSection } from "@/components/landingpage/StatsSection";
 import { TestimonialsSection } from "@/components/landingpage/TestimonialSection";
-
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-foreground overflow-hidden">
